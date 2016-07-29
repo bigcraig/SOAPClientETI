e94a88c36f3b9611484a7ee63c362a1186a21994
Craig Woollett <craigwoollett@outlook.com>
2016-07-29 14:34:03 +1000

    Add project files.

---
+++ ETI application/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETI_application
+{
+    class Program
+    {
+//DeviceDetailsStruct modem = new(DeviceDetailsStruct);
+        
+
+        static void Main(string[] args)
+        {
+          DeviceIDst
+        }
+    }
+
+    
+    
+    
+}
